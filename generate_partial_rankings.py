c82aadeae615184e7098cbc5ffa4823d887250ff
khuxkm fbexl <khuxkm@ttm.sh>
2019-10-22 07:45:27 -0400

    Add script for generating week by week ratings.

---
+++ generate_partial_rankings.py
@@ -0,0 +1,19 @@
+import csv, abbr2team, sys
+assert len(sys.argv)==3,"usage: generate_partial_rankings.py <games.csv> <week>"
+from Colley import *
+
+def adjust_ot(notes):
+	if notes=="OT":
+		return 0.75
+	return 1
+
+week = int(sys.argv[2])
+
+with open(sys.argv[1]) as f: games = [row[1:] for row in csv.reader(f) if int(row[0])<=week]
+
+c = Colley(games)
+rankings = c.process(weight_fn=adjust_ot,use_margin=True)
+
+for i,t in enumerate(rankings,1):
+	team,score = t
+	print("#{!s} {} (rating: {:.06f})".format(i,abbr2team.team_name(team),score))
